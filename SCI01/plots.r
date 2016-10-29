e8942a1b128e5ff5511c40bcf41f62b96e15fae4
Vipin Sharma <sh.vipin@gmail.com>
2016-10-29 15:54:51 -0700

    sci01: initial setup
    Adding initial setup for project for SCI 01

--- SCI01/plots.r
+++ SCI01/plots.r
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 # Collection of all plots for the data. 
 
 ggplot(air, aes(did_search)) + geom_histogram(binwidth = 5)
--- SCI01/read_data.r
+++ SCI01/read_data.r
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 library(ggplot2)
 air = read.table("air_exp.txt", sep="|", header = TRUE, na.strings = c("", "NA"))
 ggplot(air, aes(did_search)) + geom_histogram(binwidth = 5)
