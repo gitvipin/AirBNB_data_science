3740225d51c17c4503452e0d7a145cdab7876767
Vipin Sharma <sh.vipin@gmail.com>
2016-10-30 01:03:29 -0700

    sci01: adding plots
    Adding some plots.

--- SCI01/plots.r
+++ SCI01/plots.r
@@ -1,5 +1,21 @@
 #!/usr/bin/env Rscript
 
+source ("read_data.R")
 # Collection of all plots for the data. 
 
-ggplot(air, aes(did_search)) + geom_histogram(binwidth = 5)
+OUTPUT_PATH <- "./output/plots/"
+EXTENSION <- "jpeg" # "pdf"
+
+# A simple histogram of did_search
+plot <-ggplot(airdata, aes(did_search)) + geom_histogram(binwidth = 5)
+fname <- paste(OUTPUT_PATH, "did_search_histogram", ".", EXTENSION, sep="")
+ggsave(filename=fname, plot=plot)
+
+# Correlation between did_search and sent_message 
+# Show as NEGATIVE Test case in presentation as en example of how choice of variable matters for right plots ?
+plot <-ggplot(airdata, aes(did_search, sent_message)) + geom_point(color="green", size=7)
+fname <- paste(OUTPUT_PATH, "correlation_search_vs_message", ".", EXTENSION, sep="")
+ggsave(filename=fname, plot=plot, device = "jpeg")
+
+
+
--- SCI01/read_data.r
+++ SCI01/read_data.r
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 library(ggplot2)
 airdata = read.table("./data/airbnb.txt", sep="|", header = TRUE, na.strings = c("", "NA"))
-str(airdata)
-plot <-ggplot(airdata, aes(did_search)) + geom_histogram(binwidth = 5)
 
-ggsave(filename="./output/plots/myPlot.pdf", plot=plot)
+# show info about data
+str(airdata)
+names(data)
 
