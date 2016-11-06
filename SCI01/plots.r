a4087c6b8d418b32f8267a6ae22257bf2611719e
Vipin Sharma <sh.vipin@gmail.com>
2016-11-06 10:23:35 -0800

    sci01 : Add next session data into the session lengths

--- SCI01/plots.r
+++ SCI01/plots.r
@@ -11,16 +11,25 @@ plot <-ggplot(airdata, aes(did_search)) + geom_histogram(binwidth = 5)
 fname <- paste(OUTPUT_PATH, "did_search_histogram", ".", EXTENSION, sep="")
 ggsave(filename=fname, plot=plot)
 
+# A simple histogram of did_search
+plot <-ggplot(airdata, aes(dim_session_number)) + geom_density( fill="coral3") + ggtitle("Density Plot")
+  + scale_x_continuous(name="No. of repeated visits", limits=c(0, 800), breaks=seq(0,800, 100)) 
+fname <- paste(OUTPUT_PATH, "density_plot_visit_count", ".", EXTENSION, sep="")
+ggsave(filename=fname, plot=plot)
+
+# Bar with sent_message and device app combo
+plot <- ggplot(airdata, aes(sent_message, fill=dim_device_app_combo)) + geom_bar()
+fname <- paste(OUTPUT_PATH, "bar_sent_message_device_combo", ".", EXTENSION, sep="")
+ggsave(filename=fname, plot=plot)
+
+
 # Correlation between did_search and sent_message 
 # Show as NEGATIVE Test case in presentation as en example of how choice of variable matters for right plots ?
 plot <-ggplot(airdata, aes(did_search, sent_message)) + geom_point(color="green", size=7)
 fname <- paste(OUTPUT_PATH, "correlation_search_vs_message", ".", EXTENSION, sep="")
 ggsave(filename=fname, plot=plot, device = "jpeg")
 
-# This plot calculates the frequency of session lengths in histogram.
-t_start = as.numeric(as.POSIXct(airdata$ts_min, format='%Y-%m-%d %H:%M:%S'))
-t_stop = as.numeric(as.POSIXct(airdata$ts_max, format='%Y-%m-%d %H:%M:%S'))
-airdata$session_lengths = as.integer((t_stop - t_start)/60)
+
 plot <- ggplot() + aes(airdata$session_lengths) + geom_histogram(binwidth = 50, colour="black", fill="red")
 fname <- paste(OUTPUT_PATH, "session_frequency", ".", EXTENSION, sep="")
 ggsave(filename=fname, plot=plot, device = "jpeg")
@@ -38,4 +47,8 @@ plot <- ggplot(airdata, aes(dim_session_number, session_lengths, color=sent_mess
   scale_y_continuous(name="Session duration(minutes)", limits=c(0, 500), breaks=seq(0, 500, 100))
 fname <- paste(OUTPUT_PATH, "plot2", ".", EXTENSION, sep="")
 ggsave(filename=fname, plot=plot, device = "jpeg")
-  
+
+
+plot <- ggplot(airdata, aes(airdata$day, as.numeric(airdata$session_lengths))) + geom_point(color="khaki3")
+fname <- paste(OUTPUT_PATH, "day_vs_session_length", ".", EXTENSION, sep="")
+ggsave(filename=fname, plot=plot, device = "jpeg")
--- SCI01/read_data.r
+++ SCI01/read_data.r
@@ -2,7 +2,27 @@
 library(ggplot2)
 airdata = read.table("./data/airbnb.txt", sep="|", header = TRUE, na.strings = c("", "NA"))
 
+# Compute the Session Lengths based on the start and stop time stamps.
+t_start = as.numeric(as.POSIXct(airdata$ts_min, format='%Y-%m-%d %H:%M:%S'))
+t_stop = as.numeric(as.POSIXct(airdata$ts_max, format='%Y-%m-%d %H:%M:%S'))
+t_start_next = as.numeric(as.POSIXct(airdata$next_ts_min, format='%Y-%m-%d %H:%M:%S'))
+t_stop_next = as.numeric(as.POSIXct(airdata$next_ts_max, format='%Y-%m-%d %H:%M:%S'))
+airdata$session_lengths = as.integer((t_stop - t_start)/60) + as.integer((t_start_next - t_stop_next)/60)
+
+
+# Compute the Day of the week from data.
+airdata$day <- weekdays(as.Date(airdata$ds))
 # show info about data
 str(airdata)
 names(data)
 
+
+# What are the chances if someone sends a message, he/she will make a booking request.
+cor(airdata$sent_message, airdata$sent_booking_request) # 0.21
+
+# What are the chances that one will send the message if searches a property ?
+cor(airdata$did_search, airdata$sent_message) # 0.04
+
+
+# Does someone's staying more on site means increased chances of booking ? 
+cor(as.numeric(airdata$session_lengths), airdata$sent_booking_request, use="complete") # 0.15
